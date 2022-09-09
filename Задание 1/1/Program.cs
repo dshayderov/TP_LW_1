a4f1594fed9cc5631c36e0cc5587fd388a4350d6
dshayderov <dshayderov@list.ru>
2022-09-09 10:55:37 +0300

    work

---
+++ Задание 1/1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LR_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1");
+            Console.WriteLine("Наименование ЛР: Разработка консольного приложения");
+            Console.WriteLine("Выполнил: Шайдеров Дмитрий Викторович");
+            Console.WriteLine("Группа: ИВТ-б-о-21-1, 09.03.01");
+            Console.WriteLine("Дата рождения: 29.09.2003");
+            Console.WriteLine("Населенный пункт: г. Буденновск");
+            Console.WriteLine("Любимый предмет в школе: Физика");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 2/2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LR_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double t = 0.25;
+            int z = 52;
+            double f1 = 9.5;
+            int U = 12;
+
+            double g11 = t * z + z * f1 - (f1 + U) / t;
+
+            Console.WriteLine("t = {0}", t);
+            Console.WriteLine("z = {0}", z);
+            Console.WriteLine("f1 = {0}", f1);
+            Console.WriteLine("U = {0}", U);
+            Console.WriteLine("g11 = {0} * {1} + {1} * {2} - ({2} + {3})/{0} = {4}", t, z, f1, U, g11);
+
+            Console.ReadKey();
+        }
+    }
+}
